8961b65b537a64d1261bcb88756bf65f6817c634
Jannnn444 <yucianhuang@gmail.com>
2025-03-05 14:36:37 +0800

    Disabled autocorrection

--- Notes/View/ContentView.swift
+++ Notes/View/ContentView.swift
@@ -73,7 +73,7 @@ struct ContentView: View {
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
                 .frame(width: 300)
-                .autocorrectionDisabled(true)
+                .autocorrectionDisabled()
                 .textInputAutocapitalization(.none)
                 .submitLabel(.done)
                 .onSubmit {
